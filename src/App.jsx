c223cdb3b3882cfb661a385cb72c45749fb99fbb
rhitika <rhitkajuli@gmail.com>
2025-08-25 16:13:33 +0530

    Fix import path casing for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./home/home";
+import Home from "./HomeTemp/home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/courses";
 import Signup from "./components/signup";
