44d7ae4317ff2482a46e81788e429e83481c8ff4
rhitika <rhitkajuli@gmail.com>
2025-08-25 16:17:02 +0530

    Fix FreeCompany component casing

--- src/HomeTemp/home.jsx
+++ src/HomeTemp/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Banner from '../components/banner'
-import FreeCompany from '../components/FreeCompany'
+import FreeCompany from "../components/Freecompany";
 import Footer from '../components/footer'
 function Home() {
   return (
